91c57e276969d2d98c59905cacafd1dcf537225c
Ed Kelly <edwardjkelly@gmail.com>
2026-04-13 10:02:00 +0000

    Add a more complex test

--- tests/DifferTest.elm
+++ tests/DifferTest.elm
@@ -8,44 +8,42 @@ import Set
 import Test exposing (..)
 
 
-mkDictFuzzer : Fuzzer comparable -> Fuzzer v -> Fuzzer (Dict.Dict comparable v)
-mkDictFuzzer k v =
-    F.list (F.pair k v)
-        |> F.map Dict.fromList
+suite : Test
+suite =
+    describe "Round-trip tests"
+        [ dictTest ()
+        , setTest ()
+        , listTest ()
+        , complexTest ()
+        ]
 
 
-mkSetFuzzer : Fuzzer comparable -> Fuzzer (Set.Set comparable)
-mkSetFuzzer m =
-    F.list m
-        |> F.map Set.fromList
-
-
-dictTest : Test
-dictTest =
+dictTest : () -> Test
+dictTest () =
     let
-        dictDiffer =
+        differ =
             Differ.dict Differ.string Differ.int
 
-        dictFuzzer =
-            mkDictFuzzer F.string F.int
+        fuzzer =
+            dictFuzzer F.string F.int
     in
-    fuzzTest dictFuzzer dictDiffer "dict"
+    fuzzTest fuzzer differ "dict"
 
 
-setTest : Test
-setTest =
+setTest : () -> Test
+setTest () =
     let
         differ =
             Differ.set Differ.string
 
         fuzzer =
-            mkSetFuzzer F.string
+            setFuzzer F.string
     in
     fuzzTest fuzzer differ "set"
 
 
-listTest : Test
-listTest =
+listTest : () -> Test
+listTest () =
     let
         differ =
             Differ.list Differ.string
@@ -56,8 +54,8 @@ listTest =
     fuzzTest fuzzer differ "list"
 
 
-productTest : Test
-productTest =
+productTest : () -> Test
+productTest () =
     let
         differ =
             Differ.pure (\a b c -> { a = a, b = b, c = c })
@@ -72,6 +70,44 @@ productTest =
                 F.bool
     in
     fuzzTest fuzzer differ "product"
+
+
+complexTest : () -> Test
+complexTest () =
+    let
+        differ =
+            Differ.list
+                (Differ.pure (\a b c -> { a = a, b = b, c = c })
+                    |> Differ.andMap .a (Differ.dict Differ.int Differ.float)
+                    |> Differ.andMap .b (Differ.set Differ.char)
+                    |> Differ.andMap .c Differ.string
+                )
+
+        fuzzer =
+            F.list
+                (F.map3 (\a b c -> { a = a, b = b, c = c })
+                    (dictFuzzer F.int (F.floatRange 0.0 1.0))
+                    (setFuzzer F.char)
+                    F.string
+                )
+    in
+    fuzzTest fuzzer differ "complex"
+
+
+
+-- Helpers
+
+
+dictFuzzer : Fuzzer comparable -> Fuzzer v -> Fuzzer (Dict.Dict comparable v)
+dictFuzzer k v =
+    F.list (F.pair k v)
+        |> F.map Dict.fromList
+
+
+setFuzzer : Fuzzer comparable -> Fuzzer (Set.Set comparable)
+setFuzzer m =
+    F.list m
+        |> F.map Set.fromList
 
 
 fuzzTest : Fuzzer b -> Differ.Differ b -> String -> Test
